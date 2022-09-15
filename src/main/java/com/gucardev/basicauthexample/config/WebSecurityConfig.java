a1107cbdf6306d6c88d5bb3d8b3e2df3637b9e71
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-15 16:30:56 +0300

    different ways for userDetailsService

--- src/main/java/com/gucardev/basicauthexample/config/WebSecurityConfig.java
+++ src/main/java/com/gucardev/basicauthexample/config/WebSecurityConfig.java
@@ -2,12 +2,15 @@ package com.gucardev.basicauthexample.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -28,14 +31,37 @@ public class WebSecurityConfig {
         this.passwordEncoder = passwordEncoder;
     }
 
+
+
     @Bean
-    public DaoAuthenticationProvider authProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder);
-        return authProvider;
+    InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails user1 = User.withUsername("ahmet")
+                .password(passwordEncoder.encode("pass"))
+                .authorities("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1);
     }
 
+
+//    @Bean
+//    public DaoAuthenticationProvider authProvider() {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setUserDetailsService(userDetailsService);
+//        authProvider.setPasswordEncoder(passwordEncoder);
+//        return authProvider;
+//    }
+
+
+//    @Bean
+//    public AuthenticationManager authManager(HttpSecurity http)
+//            throws Exception {
+//        return http.getSharedObject(AuthenticationManagerBuilder.class)
+//                .userDetailsService(userDetailsService)
+//                .passwordEncoder(passwordEncoder)
+//                .and()
+//                .build();
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -45,14 +71,21 @@ public class WebSecurityConfig {
                 .authorizeRequests(auth -> {
                     auth.antMatchers("/api/admin").hasAuthority("ADMIN");
                     auth.antMatchers("/api/user").hasAnyAuthority("ADMIN", "USER");
-                    auth.antMatchers("/api/public", "/h2-console/**").permitAll();
+                   // auth.antMatchers("/api/public", "/h2-console/**").permitAll();
                     auth.anyRequest().authenticated();
                 })
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .httpBasic(withDefaults())
+                //.userDetailsService(userDetailsService)
+                .userDetailsService(userDetailsManager())
                 .build();
 
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/api/public", "/h2-console/**");
     }
 
     @Bean
